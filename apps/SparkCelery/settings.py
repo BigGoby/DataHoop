55667fbdf2c08ba2c632eb2a4ce976f2e04860e2
LuckyWang <ilikepython@163.com>
2019-06-23 04:07:22 +0800

    单机spark测试

--- apps/SparkCelery/settings.py
+++ apps/SparkCelery/settings.py
@@ -1,18 +1,9 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 import os
-#项目根目录
-BASEDIR = os.path.join((os.path.dirname(os.path.abspath(__file__))))
-url = "hdfs://master:9000/datahoop/"
-filepath_DIR = "hdfs://master:9000/datahoop/"                  #文件路径
-filepath_model_DIR = "hdfs://master:9000/datahoop/filepath_model/"       #模型路径
-filepath_result_DIR = "hdfs://master:9000/datahoop/filepath_result/"   #拟合或预测结果
-# mongo地址
-mongodbUri = 'mongodb://172.17.0.100:27017'
-
-#获取日志配置
 import datetime
 import logging
 import sys
+
 #日志级别大小关系为：CRITICAL > ERROR > WARNING > INFO > DEBUG > NOTSET
 def quick_start_log(modelName,log_fn=None,consoles='yes', mode=None, level=logging.DEBUG,\
                     format='%(asctime)s - %(name)s [line:%(lineno)d] - %(levelname)s - %(message)s'):
